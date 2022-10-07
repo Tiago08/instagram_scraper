bd116dd7480812ff96630c40ac4d3f2c9ed9c8b8
Tiago08 <santibeta2003@gmail.com>
2022-10-07 19:25:40 -0400

    disabled the biography extract

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -2,11 +2,11 @@
 import instaloader
 
 def print_profile(user):
-    file.write("\n" + str(user.username))
+    file.write("\n" + str(user.username))   
     file.write("\t" + str(user.followers))
     file.write("\t" + str(user.followees))
     file.write("\t" + str(user.is_verified))
-    file.write("\t" + str(user.biography).replace("\n", ""))
+    # file.write("\t" + str(user.biography).replace("\n", ""))
 
 L = instaloader.Instaloader()
 
@@ -17,23 +17,24 @@ L.login(username, password)  # (login)
 account_to_scrape = input("Paste the username you want to scrape: ")
 
 # Obtain profile metadata
-# example account: ultabeauty
 profile = instaloader.Profile.from_username(L.context, account_to_scrape)
-file = open("ulta_beauty.csv", "a+", encoding="utf-8")
+file = open(f"{profile.username}.csv", "w", encoding="utf-8")
 # Formatting the data
 file.write("User")
 file.write("\tFollowers")
 file.write("\tFollowing")
 file.write("\tVerified")
-file.write("\tBiography")
+# file.write("\tBiography")
 file.write("\n" + str(profile.username))
 file.write("\t" + str(profile.followers))
 file.write("\t" + str(profile.followees))
 file.write("\t" + str(profile.is_verified))
-file.write("\t" + str(profile.biography).replace("\n", ""))
+# file.write("\t" + str(profile.biography).replace("\n", ""))
 
 count = 0
 for followee in profile.get_followees():
+    if count >= 300:
+        break
     profile = instaloader.Profile.from_username(L.context, followee.username)
     print_profile(profile)
     print(profile.username)
