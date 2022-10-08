57bd0bad204e486267916ddaad01519a497bea79
Tiago08 <santibeta2003@gmail.com>
2022-10-07 20:50:57 -0400

    testing another way

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -1,12 +1,13 @@
 # Get instance
 import instaloader
+from instagramy import InstagramUser
 
 def print_profile(user):
-    file.write("\n" + str(user.username))   
-    file.write("\t" + str(user.followers))
-    file.write("\t" + str(user.followees))
+    file.write("\n" + str(user.username))
+    file.write("\t" + str(user.number_of_followers))
+    file.write("\t" + str(user.number_of_followings))
     file.write("\t" + str(user.is_verified))
-    # file.write("\t" + str(user.biography).replace("\n", ""))
+    file.write("\t" + str(user.biography.replace("\n", "")))
 
 L = instaloader.Instaloader()
 
@@ -17,27 +18,24 @@ L.login(username, password)  # (login)
 account_to_scrape = input("Paste the username you want to scrape: ")
 
 # Obtain profile metadata
-profile = instaloader.Profile.from_username(L.context, account_to_scrape)
-file = open(f"{profile.username}.csv", "w", encoding="utf-8")
+primary_user = instaloader.Profile.from_username(L.context, account_to_scrape)
+user = InstagramUser(account_to_scrape)
+file = open(f"{primary_user.username}.csv", "w", encoding="utf-8")
 # Formatting the data
 file.write("User")
 file.write("\tFollowers")
 file.write("\tFollowing")
 file.write("\tVerified")
-# file.write("\tBiography")
-file.write("\n" + str(profile.username))
-file.write("\t" + str(profile.followers))
-file.write("\t" + str(profile.followees))
-file.write("\t" + str(profile.is_verified))
-# file.write("\t" + str(profile.biography).replace("\n", ""))
+print_profile(user.username)
+
 
 count = 0
-for followee in profile.get_followees():
+for followee in primary_user.get_followees():
     if count >= 300:
         break
-    profile = instaloader.Profile.from_username(L.context, followee.username)
-    print_profile(profile)
-    print(profile.username)
+    user = InstagramUser(followee.username)
+    print_profile(user)
+    print(user.username)
     count += 1
 
 file.close()
--- scraper.py
+++ scraper.py
@@ -2,7 +2,16 @@
 from instagramy import InstagramUser
  
 session_id = "857410118%3AldIoHwEyCgbSBx%3A28%857410118%3AldIoHwEyCgbSBx%3A28%3AAYfDYm_soFnPjOqWZI9dmB4Nadgb1MV9f2VbvVaLTDg"
-user = InstagramUser("cristiano")
+user = InstagramUser("minabonino")
 
-print(user.user_data)
-
+file = open(f"{user.username}.csv", "w", encoding="utf-8")
+# Formatting the data
+file.write("User")
+file.write("\tFollowers")
+file.write("\tFollowing")
+file.write("\tFollows Viewer")
+file.write("\n" + str(user.username))
+file.write("\t" + str(user.number_of_followers))
+file.write("\t" + str(user.number_of_followings))
+file.write("\t" + str(user.is_verified))
+file.close()
