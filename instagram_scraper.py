9c89ee2acfd749d49dd42e9ff1985c61a821df35
Tiago08 <santibeta2003@gmail.com>
2022-10-08 08:36:14 -0400

    Added the exe file, and seted limit to 199

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -1,13 +1,13 @@
 # Get instance
 import instaloader
-from instagramy import InstagramUser
+import os
 
 def print_profile(user):
     file.write("\n" + str(user.username))
-    file.write("\t" + str(user.number_of_followers))
-    file.write("\t" + str(user.number_of_followings))
-    file.write("\t" + str(user.is_verified))
-    file.write("\t" + str(user.biography.replace("\n", "")))
+    file.write("\t" + str(user.followers))
+    file.write("\t" + str(user.followees))
+    file.write("\t" + str(user.full_name))
+    # file.write("\t" + str(user.biography.replace("\n", "")))
 
 L = instaloader.Instaloader()
 
@@ -15,30 +15,26 @@ L = instaloader.Instaloader()
 username = input("Type your instagram mail: ")
 password = input("Type your instagram password: ")
 L.login(username, password)  # (login)
+os.system('cls')
 account_to_scrape = input("Paste the username you want to scrape: ")
 
 # Obtain profile metadata
 primary_user = instaloader.Profile.from_username(L.context, account_to_scrape)
-user = InstagramUser(account_to_scrape)
 file = open(f"{primary_user.username}.csv", "w", encoding="utf-8")
 # Formatting the data
 file.write("User")
 file.write("\tFollowers")
 file.write("\tFollowing")
-file.write("\tVerified")
-print_profile(user.username)
+file.write("\tFull Name")
 
 
 count = 0
 for followee in primary_user.get_followees():
-    if count >= 300:
+    primary_user = instaloader.Profile.from_username(L.context, followee.username)
+    if count >= 199:
         break
-    user = InstagramUser(followee.username)
-    print_profile(user)
-    print(user.username)
+    print_profile(primary_user)
+    print(primary_user.username)
     count += 1
 
 file.close()
-
-# tbetan508@gmail.com
-# 35rnpZ8YfFGHTyT
--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 
 from instagramy import InstagramUser
  
-session_id = "857410118%3AldIoHwEyCgbSBx%3A28%857410118%3AldIoHwEyCgbSBx%3A28%3AAYfDYm_soFnPjOqWZI9dmB4Nadgb1MV9f2VbvVaLTDg"
 user = InstagramUser("minabonino")
 
 file = open(f"{user.username}.csv", "w", encoding="utf-8")
@@ -10,7 +9,7 @@ file.write("User")
 file.write("\tFollowers")
 file.write("\tFollowing")
 file.write("\tFollows Viewer")
-file.write("\n" + str(user.username))
+# file.write("\n" + str(user.username))
 file.write("\t" + str(user.number_of_followers))
 file.write("\t" + str(user.number_of_followings))
 file.write("\t" + str(user.is_verified))
